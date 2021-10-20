eb581c85f347452e851fdad4cbea969dd2781d02
Joachim Morken <joknumo@gmail.com>
2021-10-20 12:52:55 +0200

    Create some printer methods

--- RMI-Client/src/main/java/Client.java
+++ RMI-Client/src/main/java/Client.java
@@ -9,7 +9,11 @@ public class Client {
 
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
         PrinterInterface service = (PrinterInterface) Naming.lookup("rmi://localhost:8081/printer");
-        System.out.println(service.echo("hey server"));
+        System.out.println(service.status("printer1"));
+        System.out.println(service.queue("printer1"));
+        System.out.println(service.print("file1", "printer1"));
+        System.out.println(service.topQueue("printer1", 0));
+        System.out.println(service.restart());
     }
 
 }
--- RMI-Printer-Server/src/main/java/PrinterServant.java
+++ RMI-Printer-Server/src/main/java/PrinterServant.java
@@ -1,49 +1,66 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class PrinterServant extends UnicastRemoteObject implements PrinterInterface {
 
+    HashMap<String, Printer> printers;
+
+
     public PrinterServant() throws RemoteException{
         super();
-    }
-
-    public String echo(String input) throws RemoteException {
-    return "From server: " + input;
+        printers = new HashMap<>();
+        Printer printer1 = new Printer("printer1", List.of("file1", "file2"));
+        Printer printer2 = new Printer("printer2", List.of("file3", "file4"));
+        printers.put("printer1", printer1);
+        printers.put("printer2", printer2);
     }
 
     @Override
     public String print(String filename, String printer) throws RemoteException {
-        return null;
+        return printers.get(printer).print(filename);
     }
 
     @Override
     public String queue(String printer) throws RemoteException {
-        return null;
+        return printers.get(printer).queue();
     }
 
     @Override
-    public void topQueue(String printer, int job) throws RemoteException {
-
+    public String topQueue(String printer, int job) throws RemoteException {
+        return printers.get(printer).topQueue(job);
     }
 
     @Override
     public void start() throws RemoteException {
-
+        System.out.println("Starting server...");
     }
 
     @Override
     public void stop() throws RemoteException {
-
+        System.out.println("Shutting down server...");
     }
 
     @Override
-    public void restart() throws RemoteException {
-
+    public String restart() throws RemoteException {
+        StringBuilder b = new StringBuilder();
+        b.append("Shutting down server...\n");
+        b.append("Clearing print queue...\n");
+        for (Printer printer : this.printers.values()) {
+            printer.clear();
+        }
+        b.append("Print queue cleared\n");
+        b.append("Starting server...\n");
+        return String.valueOf(b);
     }
 
-    @Override
     public String status(String printer) throws RemoteException {
-        return null;
+        if (printers.containsKey(printer)) {
+            return "Printer is online + \n" + printers.get(printer).status();
+        }
+            return "Printer is offline";
     }
 
     @Override
